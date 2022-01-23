c9a97ae71c78088f7dbb506cb2c67a550d28f480
Codeboy6496 <88576631+Codeboy6496@users.noreply.github.com>
2022-01-23 13:40:16 +0530

    Add files via upload

---
+++ game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number, guess, nGuesses = 1;
+    srand(time(0));
+    // srand(1);
+    number = rand() % 100 + 1;
+
+    do
+    {
+        printf("Guess the number between 1 to 100 : ");
+        scanf("%d", &guess);
+
+        if (guess > number)
+        {
+            printf("Lower number please\n");
+        }
+        else if (guess < number)
+        {
+            printf("Greater number please\n");
+        }
+        else
+        {
+            printf("Yay you got it!\nYou took %d attempts", nGuesses);
+        }
+        nGuesses++;
+
+    } while (guess != number);
+
+    return 0;
+}
